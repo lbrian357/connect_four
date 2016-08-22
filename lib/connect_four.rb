dd4b8be01e4b3e3acd95b076424e8a58804f7ff2
brian <lbrian357@gmail.com>
2016-08-22 17:48:44 +0300

    features added, tests with gets.chomp should be added

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,5 +1,13 @@
+class Player
+  attr_accessor :piece, :victory
+  def initialize(piece)
+    @piece = piece
+    @victory = false
+  end
+end
+
 class Game 
-  attr_accessor :row1, :row2, :row3, :row4, :row5, :row6, :row_ary
+  attr_accessor :row1, :row2, :row3, :row4, :row5, :row6, :p1, :p2
   def initialize
     @row1 = "| | | | | | | |\n---------------\n"
     @row2 = "| | | | | | | |\n---------------\n"
@@ -7,7 +15,8 @@ class Game
     @row4 = "| | | | | | | |\n---------------\n"
     @row5 = "| | | | | | | |\n---------------\n"
     @row6 = "| | | | | | | |\n---------------\n"
-    @row_ary = [@row1, @row2, @row3, @row4, @row5, @row6]
+    @p1 = Player.new('O')
+    @p2 = Player.new('@')
   end
 
   def p_board
@@ -30,56 +39,176 @@ class Game
       11
     when 7
       13
-    else
-      puts 'not an option'
     end
   end
 
-  def check_col(col)
+  def target_col(col) #returns the row that has the next free slot
+    row_ary = [row1, row2, row3, row4, row5, row6]
     i = 0
     condition = false
     while i < row_ary.length || condition == false
       if row_ary[i][find_col(col)] == ' '
         return row_ary[i]
-        #row_ary[i][find_col(col)] = 'O'
         condition = true
       end
       i += 1
-=begin
-      if i == row_ary.length
-        puts 'you cannot do that, try again'
-      end
-=end
     end
-    condition
   end
 
-  def target_col(col)
+  def find_last_filled_col(col)
+    row_ary = [row1, row2, row3, row4, row5, row6]
     i = 0
     condition = false
-    while i < @row_ary.length || condition == false
-      if @row_ary[i][find_col(col)] == ' '
-        return @row_ary.index(@row_ary[3])
-        #row_ary[i][find_col(col)] = 'O'
+    while condition == false
+      if i == row_ary.length 
+        return row_ary[-1]
+        condition = true
+      elsif row_ary[i][find_col(col)] == ' '
+        return row_ary[i-1]
         condition = true
       end
       i += 1
-=begin
-      if i == row_ary.length
-        puts 'you cannot do that, try again'
-      end
-=end
     end
   end
 
-  def turn(col)
-    check_col(col)
+  def turn(col, player)
+    target_col(col)[find_col(col)] = player.piece
+  end
+
+  def start
+    count = 1
+    p_board if count == 1
+    while count <= 42 && p1.victory == false && p2.victory == false
+      if count.odd? 
+        puts 'Player 1 which column?'
+        begin
+          col = gets.chomp.to_i
+          puts 'you can\'t do that, try again' if col < 1 || col > 7 || row6[find_col(col)] != ' '
+        end while col < 1 || col > 7 || row6[find_col(col)] != ' '
+        turn(col, p1) 
+        win?(p1, col)
+
+      else
+        puts 'Player 2 which column?'
+        begin
+          col = gets.chomp.to_i
+          puts 'you can\'t do that, try again' if col < 1 || col > 7
+        end while col < 1 || col > 7 
+        turn(col, p2)
+        win?(p2, col)
+      end
+      p_board
+      count += 1
+    end
+    if count == 43
+      puts 'It\'s a draw'
+    elsif p1.victory == true
+      puts 'Player 1 wins'
+    elsif p2.victory == true
+      puts 'Player 2 wins'
+    end
+  end
+
+  def horizontal_win(player)
+    #four horizontal pieces the same
+    #it brute forces by checking all possible cases
+    row_ary = [row1, row2, row3, row4, row5, row6]
+    row_ary.each do |row|
+      base_ary = [1,2,3,4]
+      until base_ary.include?(8)
+        if row[find_col(base_ary[0])] == player.piece && row[find_col(base_ary[0])] == row[find_col(base_ary[1])] && row[find_col(base_ary[0])] == row[find_col(base_ary[2])] && row[find_col(base_ary[0])] == row[find_col(base_ary[3])] 
+          player.victory = true
+        end
+        base_ary.map! { |n| n + 1 }
+      end
+    end
+  end
+
+  def vertical_win(col, player)
+    row_ary = [row1, row2, row3, row4, row5, row6].reverse
+    check_ary = []
+    check_ary << row_ary.index(find_last_filled_col(col)) #finds highest piece in the column, presumably the one played this same turn
+    check_ary << check_ary[0] + 1
+    check_ary << check_ary[0] + 2
+    check_ary << check_ary[0] + 3
+    if check_ary.include?(6) #assures that method does not check for values outside the board
+    else
+      if row_ary[check_ary[0]][find_col(col)] == player.piece && row_ary[check_ary[0]][find_col(col)] == row_ary[check_ary[1]][find_col(col)] && row_ary[check_ary[0]][find_col(col)] == row_ary[check_ary[2]][find_col(col)] && row_ary[check_ary[0]][find_col(col)] == row_ary[check_ary[3]][find_col(col)]
+        player.victory = true
+      end
+    end
+
+  end
+
+  def r_diag_win(player)
+    row_ary = [row1, row2, row3, row4, row5, row6]
+    col_n = [1, 2, 3, 4, 5, 6, 7]
+    row_ary.each do |row|
+      check_row = []
+      check_row << row_ary.index(row)
+      check_row << check_row[0] + 1
+      check_row << check_row[0] + 2
+      check_row << check_row[0] + 3
+      col_n.each do |col|
+        check_col = []
+        check_col << col_n.index(col)
+        check_col << check_col[0] + 1
+        check_col << check_col[0] + 2
+        check_col << check_col[0] + 3
+
+        unless check_row.include?(6) || check_col.include?(7)
+          player.victory = true if row_ary[check_row[0]][find_col(col_n[check_col[0]])] == player.piece && row_ary[check_row[0]][find_col(col_n[check_col[0]])] == row_ary[check_row[1]][find_col(col_n[check_col[1]])] && row_ary[check_row[0]][find_col(col_n[check_col[0]])] == row_ary[check_row[2]][find_col(col_n[check_col[2]])] && row_ary[check_row[0]][find_col(col_n[check_col[0]])] == row_ary[check_row[3]][find_col(col_n[check_col[3]])]
+        end
+      end
+    end
+  end
+
+  def l_diag_win(player)
+    row_ary = [row1, row2, row3, row4, row5, row6]
+    col_n = [1, 2, 3, 4, 5, 6, 7].reverse
+    row_ary.each do |row|
+      check_row = []
+      check_row << row_ary.index(row)
+      check_row << check_row[0] + 1
+      check_row << check_row[0] + 2
+      check_row << check_row[0] + 3
+      col_n.each do |col|
+        check_col = []
+        check_col << col_n.index(col)
+        check_col << check_col[0] + 1
+        check_col << check_col[0] + 2
+        check_col << check_col[0] + 3
+
+        unless check_row.include?(6) || check_col.include?(7)
+          player.victory = true if row_ary[check_row[0]][find_col(col_n[check_col[0]])] == player.piece && row_ary[check_row[0]][find_col(col_n[check_col[0]])] == row_ary[check_row[1]][find_col(col_n[check_col[1]])] && row_ary[check_row[0]][find_col(col_n[check_col[0]])] == row_ary[check_row[2]][find_col(col_n[check_col[2]])] && row_ary[check_row[0]][find_col(col_n[check_col[0]])] == row_ary[check_row[3]][find_col(col_n[check_col[3]])]
+        end
+      end
+    end
+  end
+
+
+
+
+
+
+  def win?(player, col = nil)
+    if player.victory == false
+      horizontal_win(player)
+    end
+    if player.victory == false && col != nil
+      #vertical win
+      vertical_win(col, player)
+    end
+    if player.victory == false
+      r_diag_win(player)
+
+    end
+    if player.victory == false
+      l_diag_win(player)
+
+    end
   end
 end
 
 a = Game.new
-
-a.p_board
-
-
-
+a.start
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,7 +2,7 @@ require 'connect_four'
 
 describe 'Game' do
   before(:each) do
-  @a_game = Game.new
+    @a_game = Game.new
   end
 
   describe '#p_board' do
@@ -33,28 +33,190 @@ describe 'Game' do
   describe '#target_col' do
     context 'when a column number 1 is entered' do
       it 'will return the highest row number (2) that does not have a piece in it' do
-        @a_game.row2 = "| |O| | | | | |\n---------------\n"
-        @a_game.row1 = "|O|O| |O| | | |\n---------------\n"
-        expect(@a_game.target_col(1)).to eql("| |O| | | | | |\n---------------\n")
+        @a_game.row3 = "| | |O| | | | |\n---------------\n"
+        @a_game.row2 = "| |O|O| | | | |\n---------------\n"
+        @a_game.row1 = "|O|O|O| | | | |\n---------------\n"
+        expect(@a_game.target_col(1)).to eql("| |O|O| | | | |\n---------------\n")
       end
     end
 
     context 'when column 2 is entered' do
       it 'will return row3' do
+        @a_game.row3 = "| | |O| | | | |\n---------------\n"
+        @a_game.row2 = "| |O|O| | | | |\n---------------\n"
+        @a_game.row1 = "|O|O|O| | | | |\n---------------\n"
+        expect(@a_game.target_col(2)).to eql("| | |O| | | | |\n---------------\n")
+      end
+    end
+  end
+
+  describe '#turn' do
+    context 'when a column number is entered' do
+      it 'will insert a piece at the bottom-most empty row' do
+        @a_game.row1 = "|O|O|O| | | | |\n---------------\n"
+        @a_game.turn(3, @a_game.p1)
+        expect(@a_game.row2).to eql("| | |O| | | | |\n---------------\n")
+      end
+    end
+
+    context 'when a column number is entered' do
+      it ' will insert a piece at the top row if all others in the column is filled' do
+        @a_game.row6 = "| | | | | | | |\n---------------\n"
+        @a_game.row5 = "| | |O| | | | |\n---------------\n"
+        @a_game.row4 = "| | |O| | | | |\n---------------\n"
+        @a_game.row3 = "| | |O| | | | |\n---------------\n"
+        @a_game.row2 = "| | |O| | | | |\n---------------\n"
+        @a_game.row1 = "| | |O| | | | |\n---------------\n"
+
+        @a_game.turn(3, @a_game.p1)
+        expect(@a_game.row6).to eql("| | |O| | | | |\n---------------\n")
+      end
+    end
+  end
+
+  describe '#p1' do
+    context 'when a Game is initialized' do
+      it 'will create a player and assign them playing pieces' do
+        expect(@a_game.p1.piece).to eql("O")
+      end
+    end
+  end
+
+  describe '#p2' do
+    context 'when Game is initialized' do
+      it 'will create another player and assign them playing pieces' do
+        expect(@a_game.p2.piece).to eql("@")
+      end
+    end
+  end
+
+  describe '#find_last_filled_col' do
+    context 'when given a column number' do
+      it 'returns that last row with a piece in it' do
+        @a_game.row6 = "|O| | | | | | |\n---------------\n"
+        @a_game.row5 = "|O|O| | | | | |\n---------------\n"
+        @a_game.row4 = "|O|O|O| | | | |\n---------------\n"
+        @a_game.row3 = "|O|O|O|O| | | |\n---------------\n"
+        @a_game.row2 = "|O|O|O|O| | | |\n---------------\n"
+        @a_game.row1 = "|O|O|O|O| | | |\n---------------\n"
+        expect(@a_game.find_last_filled_col(2)).to equal(@a_game.row5)
+      end
+
+      it 'returns the last row' do 
+        @a_game.row6 = "|O| | | | | | |\n---------------\n"
+        @a_game.row5 = "|O|O| | | | | |\n---------------\n"
+        @a_game.row4 = "|O|O|O| | | | |\n---------------\n"
+        @a_game.row3 = "|O|O|O|O| | | |\n---------------\n"
+        @a_game.row2 = "|O|O|O|O| | | |\n---------------\n"
+        @a_game.row1 = "|O|O|O|O| | | |\n---------------\n"
+        expect(@a_game.find_last_filled_col(1)).to equal(@a_game.row6)
+      end
+    end
+  end
+
+
+  describe '#win?' do
+    context 'when there are four pieces in a horizontal row' do
+      it 'changes that player\'s victory to true' do
+        @a_game.row1 = "|O|O|O|O| | | |\n---------------\n"
+        @a_game.win?(@a_game.p1)
+        expect(@a_game.p1.victory).to eql(true)
+      end
+
+      it 'also works for row6' do
+        @a_game.row6 = "| | | |@|@|@|@|\n---------------\n"
+        @a_game.win?(@a_game.p2)
+        expect(@a_game.p2.victory).to eql(true)
+      end
+    end
+
+    context 'when there are no cases which match' do
+      it 'victory condition remains false' do
+        @a_game.row1 = "| |O| | | | | |\n---------------\n"
+        @a_game.win?(@a_game.p1, 2)
+        expect(@a_game.p1.victory).to eql(false)
+      end
+    end
+
+    context 'when there are four verticles' do
+      it 'makes victory condition true for the player' do
+        @a_game.row4 = "| | | | | | | |\n---------------\n"
+        @a_game.row3 = "| |O| | | | | |\n---------------\n"
         @a_game.row2 = "| |O| | | | | |\n---------------\n"
-        @a_game.row1 = "|O|O| |O| | | |\n---------------\n"
-        expect(@a_game.target_col(2)).to eql("| | | | | | | |\n---------------\n")
+        @a_game.row1 = "| |O| | | | | |\n---------------\n"
+        @a_game.turn(2, @a_game.p1)
+        @a_game.win?(@a_game.p1, 2)
+
+        expect(@a_game.p1.victory).to eql(true)
+      end
+    end
+
+    context 'when there are four verticles at the very top' do
+      it 'makes victory condition true for the player' do
+        @a_game.row6 = "| | | | | | | |\n---------------\n"
+        @a_game.row5 = "| |O| | | | | |\n---------------\n"
+        @a_game.row4 = "| |O| | | | | |\n---------------\n"
+        @a_game.row3 = "| |O| | | | | |\n---------------\n"
+        @a_game.row2 = "| |@| | | | | |\n---------------\n"
+        @a_game.row1 = "| |@| | | | | |\n---------------\n"
+        @a_game.turn(2, @a_game.p1)
+
+        @a_game.win?(@a_game.p1, 2)
+
+        expect(@a_game.p1.victory).to eql(true)
+      end
+    end
+
+    context 'when there are four diagonals going towards the top right' do
+      it 'makes victory ondition true for that player' do
+        @a_game.row4 = "| |@| |O| | | |\n---------------\n"
+        @a_game.row3 = "| |@|O| | | | |\n---------------\n"
+        @a_game.row2 = "| |O| | | | | |\n---------------\n"
+        @a_game.row1 = "|O|@| | | | | |\n---------------\n"
+        @a_game.win?(@a_game.p1)
+        expect(@a_game.p1.victory).to eql(true)
+      end
+    end
+
+    context 'when diagonal starts in the middle of the grid' do
+      it 'makes victory condition true for that player' do
+        @a_game.row6 = "| |@| |O| |@| |\n---------------\n"
+        @a_game.row5 = "| |@| |O|@| | |\n---------------\n"
+        @a_game.row4 = "| |@| |@| | | |\n---------------\n"
+        @a_game.row3 = "| | |@|O| | | |\n---------------\n"
+        @a_game.win?(@a_game.p2)
+        expect(@a_game.p2.victory).to eql(true)
+      end
+    end
+
+    context 'when diagonal starts from bottom right to top left' do
+      it 'makes players victory condition true' do
+        @a_game.row4 = "| | | |O| |@| |\n---------------\n"
+        @a_game.row3 = "| |@| | |O|@| |\n---------------\n"
+        @a_game.row2 = "| |@| | | |O| |\n---------------\n"
+        @a_game.row1 = "| |@| | | |@|O|\n---------------\n"
+        @a_game.win?(@a_game.p1)
+        expect(@a_game.p1.victory).to eql(true)
+      end
+    end
+
+    context 'when diagonal appears at top left corner of grid' do
+      it 'makes players victory condition true' do
+        @a_game.row6 = "|@| | | | |@| |\n---------------\n"
+        @a_game.row5 = "| |@| |O| | | |\n---------------\n"
+        @a_game.row4 = "| | |@| | |@| |\n---------------\n"
+        @a_game.row3 = "| | | |@| |@| |\n---------------\n"
+        @a_game.win?(@a_game.p2)
+        expect(@a_game.p2.victory).to eql(true)
       end
     end
   end
 =begin
-  describe '#turn' do
-    context 'when a column number is entered' do
-      it 'will insert a piece at the bottom of the row' do
-      end
-    end
-  end
+  describe '#start' do
+    context 'when a game is started' do
+      it 'will alternating between player\'s turns up to 42 times' do
+        expect(@a_game.start).to eql(
 =end
-  #expect(@a_game.row1).to eql("|O| | | | | | |\n---------------\n")
+
 end
 
